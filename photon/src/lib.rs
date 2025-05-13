eb31b8a92f5e44bdb8deb0b926bc7bac9ffaa3e6
Kristinn Vikar <kristinn@syndis.is>
2025-05-13 14:17:49 +0000

    fmt again

--- photon/src/lib.rs
+++ photon/src/lib.rs
@@ -166,12 +166,8 @@ fn init_functions() -> FxHashMap<String, DslFunction> {
                 let decoded_vec = BASE64_STANDARD.decode(inp).map_err(|_| ())?; // TODO: Don't map err, use some proper DSL error handling
                 let res = String::from_utf8(decoded_vec); // TODO: possibly needs to return raw bytes (not supported in DSL right now) instead of valid UTF-8
                 match res {
-                    Ok(decoded_str) => {
-                        Ok(Value::String(decoded_str))
-                    },
-                    Err(_) => {
-                        Err(())
-                    }
+                    Ok(decoded_str) => Ok(Value::String(decoded_str)),
+                    Err(_) => Err(()),
                 }
             }),
         ),

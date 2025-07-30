4bfae0af65ee0e7986a99acbf85bc2a59ffc0204
Kristinn Vikar <kristinn@aftra.io>
2025-07-30 16:30:29 +0000

    missing angle bracket

--- photon/src/template_loader.rs
+++ photon/src/template_loader.rs
@@ -132,7 +132,7 @@ fn parse_classification(yaml: &Yaml) -> Option<Classification> {
         Yaml::Array(cves) => cves
             .iter()
             .map(|item| item.as_str().map(String::from))
-            .collect::<Option<Vec<_>>()?,
+            .collect::<Option<Vec<_>>>()?,
         _ => vec![],
     };
     let cwe_id = match &yaml["cwe-id"] {
